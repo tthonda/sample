a4d5831ee880ead137a7f790a9637327c5cd518f
tthonda <inform@trialpc.net>
2018-12-28 22:41:04 +0900

    practice code. use hamcrest 2.1

---
+++ sample/src/test/java/net/tthonda/sample/AppTest.java
@@ -0,0 +1,20 @@
+package net.tthonda.sample;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	@Test
+	public void testApp() {
+		assertThat("test", is("test"));
+	}
+}
